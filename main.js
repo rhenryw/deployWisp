a51314de5c7c5f72bbe78fcdd1118942b8edc172
tf7software <rhenrywarren@gmail.com>
2025-04-24 16:43:31 -0700

    fix: improve request handling and formatting in main.js

--- main.js
+++ main.js
@@ -5,9 +5,19 @@ const wisp = require("wisp-server-node");
 const httpServer = http.createServer();
 
 httpServer.on("upgrade", (req, socket, head) => {
-    // please include thr trailing slash
-    if (req.url.endsWith("/wisp/")) wisp.routeRequest(req, socket, head);
-    else socket.end();
+    // please include the trailing slash
+    if (req.url.endsWith("/wisp/")) {
+        wisp.routeRequest(req, socket, head);
+    } else {
+        // Redirect to https://rhw.one
+        socket.write(
+            "HTTP/1.1 302 Found\r\n" +
+            "Location: https://rhw.one\r\n" +
+            "Connection: close\r\n" +
+            "\r\n"
+        );
+        socket.end();
+    }
 });
 
 httpServer.on("listening", () => {
