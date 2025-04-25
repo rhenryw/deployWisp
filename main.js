20f1f195d423b051e4d874863c0e42df0c06e332
tf7software <rhenrywarren@gmail.com>
2025-04-24 19:25:24 -0700

    Update to new wisp

--- main.js
+++ main.js
@@ -1,29 +1,24 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+const { server: wisp, logging } = require("@mercuryworkshop/wisp-js/server");
 const http = require("node:http");
-const wisp = require("wisp-server-node");
 
-const httpServer = http.createServer();
-
-httpServer.on("upgrade", (req, socket, head) => {
-    // please include the trailing slash
-    if (req.url.endsWith("/wisp/")) {
-        wisp.routeRequest(req, socket, head);
-    } else {
-        // Redirect to https://rhw.one
-        socket.write(
-            "HTTP/1.1 302 Found\r\n" +
-            "Location: https://rhw.one\r\n" +
-            "Connection: close\r\n" +
-            "\r\n"
-        );
-        socket.end();
-    }
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("wisp server js rewrite");
 });
 
-httpServer.on("listening", () => {
-    console.log("HTTP server listening");
+logging.set_level(logging.DEBUG);
+wisp.options.port_whitelist = [
+  [5000, 6000],
+  80,
+  443
+]
+
+server.on("upgrade", (req, socket, head) => {
+  wisp.routeRequest(req, socket, head);
 });
 
-httpServer.listen({
-    port: 8080,
+server.on("listening", () => {
+  console.log("HTTP server listening");
 });
+
+server.listen(8080, "127.0.0.1");
