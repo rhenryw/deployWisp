ab1905d58c8e99fa55f6d1fb3277cddd52553d83
tf7software <rhenrywarren@gmail.com>
2025-04-26 18:11:36 -0700

    feat: update installation script to use pm2 for application management

--- install.sh
+++ install.sh
@@ -15,20 +15,31 @@ echo "Installing Node.js and npm..."
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs
 
-# Clone the repository
+# Install pm2 
+echo "Installing pm2..."
+sudo npm install -g pm2
+
+# Clone the repo
 REPO_URL="https://github.com/rhenryw/deployWisp.git"
 TARGET_DIR="deployWisp"
 
 echo "Cloning repository from $REPO_URL..."
 git clone $REPO_URL
 
-# Navigate into the cloned repository
+# Go into dir of repo
 cd $TARGET_DIR
 
-# Install npm dependencies
+# Install dependencies
 echo "Installing npm dependencies..."
 npm install
 
-# Start the application
-echo "Starting the application..."
-npm start
+# Start the  pm2
+echo "Starting the application with pm2..."
+pm2 start npm --name "deployWisp" -- start
+
+# Set up pm2 to start on boot
+echo "Setting up pm2 to start on boot..."
+pm2 startup
+pm2 save
+
+echo "Application has been set up to start on boot and run indefinitely."
